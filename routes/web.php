a42c6de3c9d4978666d9b35f6a1f1d41d70d1cfa
Yu-Min Peng <FateXRebirth@gmail.com>
2018-03-20 09:17:45 +0800

    Models & Database Migrations

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,5 @@ Route::get('/about', function() {
 Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/service', 'PagesController@service');
+
+Route::resource('posts', 'PostsController');
