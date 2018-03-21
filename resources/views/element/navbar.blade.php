7e3fc11f76325a9248086aeb60323314885853e5
Yu-Min Peng <FateXRebirth@gmail.com>
2018-03-21 09:54:06 +0800

    Fetching Data With Eloquent

--- resources/views/element/navbar.blade.php
+++ resources/views/element/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
+    <a class="navbar-brand" href="/">{{ config('app.name') }}</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -14,6 +14,9 @@
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="/service">Service</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/posts">Blog</a>
             </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.app')
+
+@section('content')
+    <h3>Posts</h3>
+    @if(count($posts) > 0)
+        @foreach($posts as $post)
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title"><a href="/posts/{{$post->id}}">{{ $post->title }}</a></h5>
+                    <h6 class="card-subtitle mb-2 text-muted">{{$post->author}}</h6>
+                    <p class="card-text">{{ $post->body }} </p>
+                    <small> Written on {{ $post->created_at }} </small>
+                </div>
+            </div>
+        @endforeach
+        {{--  {{ $posts->links() }}  --}}
+    @else
+        <p>No posts found</p>
+    @endif
+@endsection
